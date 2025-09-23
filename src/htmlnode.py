c2cd56db9976344b2cde4b08d3bab51c86dc1813
almuiz021 <mrabdulmuizghori@gmail.com>
2025-09-23 11:39:13 +0530

    Code Refactor >> Add Test Cases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,19 +2,20 @@ class HTMLNode:
     def __init__(self, tag = None, value = None, children = None, props = None ):
         self.tag = tag
         self.value = value
-        self.children = children if children is not None else []
-        self.props = props if props is not None else {} 
+        self.children = children
+        self.props = props
 
     def to_html(self):
         raise NotImplementedError("")
     
     def props_to_html(self):
-        if self.props:
-            attributes = ""
-            for keys,value in self.props.items():
-                attribute = f' {keys} = "{value}"'
-                attributes += attribute
-            return attributes
+        if self.props is None:
+            return ""
+        attributes = ""
+        for keys,value in self.props.items():
+            attribute = f' {keys}="{value}"'
+            attributes += attribute
+        return attributes
     
     def __repr__(self):
             return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
@@ -29,10 +30,7 @@ class LeafNode(HTMLNode):
             raise ValueError("MUST HAVE VALUE")
         if self.tag is None:
             return self.value
-        attributes = self.props_to_html()    
-        if attributes is None:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
-        return f"<{self.tag}{attributes}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
      
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props = None):
@@ -48,24 +46,5 @@ class ParentNode(HTMLNode):
         for item in self.children:
             childrenTag += item.to_html()
 
-        parent_props = self.props_to_html()
-
-        if parent_props:
-            return f"<{self.tag}{self.props_to_html()}>{childrenTag}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{childrenTag}</{self.tag}>"
         
-        return f"<{self.tag}>{childrenTag}</{self.tag}>"
-        
-
-
-node = ParentNode(
-    "p",
-    [
-        LeafNode("b", "Bold text",{"class":"mx-5","ref":"someref"}),
-        LeafNode(None, "Normal text", {"this":"is wrong"} ),
-        LeafNode("i", "italic text"),
-        LeafNode(None, "Normal text"),
-    ],
-    {"class":"mx-10"}
-)
-
-print(node.to_html())
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    TEXT = "plain"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -22,3 +23,28 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    type = text_node.text_type
+    
+    if type not in TextType:
+        raise Exception("Give Right Type")
+
+    if type == TextType.TEXT:
+        return LeafNode(None,text_node.text)
+
+    if type == TextType.BOLD:
+        return LeafNode("b",text_node.text)
+
+    if type == TextType.ITALIC:
+        return LeafNode("i",text_node.text)
+
+    if type == TextType.CODE:
+        return LeafNode("code",text_node.text)
+
+    if type == TextType.LINK:
+        return LeafNode("a",text_node.text,{"href": text_node.url})
+
+    if type == TextType.IMAGE:
+        return LeafNode("img","",{"src": text_node.url,"alt":text_node.text})
