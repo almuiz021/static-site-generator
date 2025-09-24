08d2344ef6399bbe7aa25f646a6eb44c16dab24d
almuiz021 <mrabdulmuizghori@gmail.com>
2025-09-24 20:20:45 +0530

    Add BlockType With TestCases

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextType, TextNode 
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from markdown_inline import split_nodes_delimiter, extract_markdown_images,extract_markdown_links
-
+from markdown_blocks import block_to_block_type
 def main():
     
     dummy = TextNode("Some Text",TextType.LINK,"https://www.muiz.com")
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def markdown_to_blocks(markdown):
     splitted_lines = markdown.split("\n\n")
     new_lines = []
@@ -6,3 +8,51 @@ def markdown_to_blocks(markdown):
         if stripped:
             new_lines.append(stripped)
     return new_lines
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    
+    return BlockType.PARAGRAPH
+
+
