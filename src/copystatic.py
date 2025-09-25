0f524fc240c8a0095262387132567866bd84a959
almuiz021 <mrabdulmuizghori@gmail.com>
2025-09-25 19:36:15 +0530

    Add Recursive Pages Generator

--- src/copystatic.py
+++ src/copystatic.py
@@ -41,10 +41,36 @@ def generate_page(from_path, template_path, dest_path):
 
     replaced_content = replaced_title.replace("{{ Content }}", html.to_html())
 
-    # os.makedirs(dest_path,exist_ok=True)
-
     with open(dest_path, "w") as to_write:
         all_content = to_write.write(replaced_content)
 
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dir_exists = os.path.exists(dir_path_content)
+    template_exists = os.path.exists(template_path)
+    if not dir_exists:
+        raise Exception("Content Directory Not Available")
+    
+    if not template_exists:
+        raise Exception("Template Not Available")
+     
+    os.makedirs(dest_dir_path, exist_ok=True)
+    
+    list_content = os.listdir(dir_path_content)
+    print(list_content)
+
+    if list_content:
+        for each in list_content:
+            print("thisis",)
+            content_inside = os.path.join(dir_path_content,each)
+            public_inside = os.path.join(dest_dir_path,each)
+
+            if os.path.isfile(content_inside):
+                html_ext = each.replace(".md",".html")
+                print(html_ext)
+                html_file = os.path.join(dest_dir_path,html_ext)
+                generate_page(content_inside, template_path, html_file)
+            else:
+                generate_pages_recursive(content_inside,template_path,public_inside)
 
     
--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
-from copystatic import copystatic, generate_page
+from copystatic import copystatic, generate_page, generate_pages_recursive
 
 
 
 def main():
     print("main")
     copystatic("static/","public/")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    # generate_page("content/index.md", "template.html", "public/index.html")
+
+    generate_pages_recursive("content/", "template.html", "public/")
+    
  
 if __name__ == "__main__":
     main()
